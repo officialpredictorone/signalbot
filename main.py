f4af560e53fde8b22584c095cce28fbd876fac95
Brian Hacker <brianhackerr01@gmail.com>
2025-09-21 17:58:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from aiogram import F
 from aiogram.client.default import DefaultBotProperties
 import logging
 
-TOKEN = "8160440178:AAENyedvsEdYdxkAnePFE8SeofMUGbyag_c"
+TOKEN = "8290197550:AAEXSyA1LsQuBnu2tvIp6w1N4CJgNGzFKGU"
 
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
 dp = Dispatcher(storage=MemoryStorage())
